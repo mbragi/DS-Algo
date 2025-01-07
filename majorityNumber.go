ddb230fd3f3963a588037dc92dc2ffef5f7f61ea
mbragi <spontaneousmanji@gmail.com>
2025-01-07 03:35:43 +0100

    chore: find majority number and its count

---
+++ majorityNumber.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Function to find majority number and its count
+func findMajorityNumberAndFrequencyCount(arr []int) map[string]interface{} {
+	if len(arr) == 0 {
+		return map[string]interface{}{"majority": nil, "count": 0}
+	}
+
+	freqMap := make(map[int]int)
+	n := len(arr)
+	for _, num := range arr {
+		if freqMap[num] > int(math.Floor(float64(n)/2)) {
+			return map[string]interface{}{"majority": num, "count": freqMap[num]}
+		}
+		freqMap[num]++
+	}
+	return map[string]interface{}{"majority": nil, "count": 0}
+}
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 5}
+	result := findMajorityNumberAndFrequencyCount(arr)
+	fmt.Println(result)
+}
